a72a8cfc164aa6dde3895e803f9a3412a4776013
sadique <siisadique572@gmail.com>
2026-04-28 12:16:42 +0530

    First commit

--- git-script.sh
+++ git-script.sh
@@ -2,6 +2,8 @@
 
 # phle hum git banayenge
 
+read -p "yhn apna token dalo" token
+
 git init
 
 git status
@@ -9,3 +11,7 @@ git status
 git add .
 
 git commit -m "First commit"
+
+git remote add set-url https://$token@https://github.com/siisadique572-source/shell-scripting.git
+
+git push -f origin master
